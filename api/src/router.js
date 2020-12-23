89fc7e8ca3b829e883333521f0b8aca162227deb
Mark Buynoski <Mark.Buynoski@transport4.com>
2020-12-23 10:56:11 -0500

    Completed module

---
+++ api/src/router.js
@@ -0,0 +1,40 @@
+const { Router } = require('express');
+const multer = require('multer');
+const router = Router();
+
+const storage= multer.diskStorage({
+    destination: 'api/uploads',
+    filename='filename'
+});
+
+const upload = multer({
+    fileFilter: fileFilter(),
+    storage: storage
+});
+
+router.post('/upload', upload.single('photo'),(request, response) =>{
+    if(request.fileValidationError !== undefined){
+        return response.status(400).json({
+            error: request.fileValidationError
+        });
+    }else{
+        response.status(201).json({
+            success: true
+        });
+    }
+});
+
+module.exports= router;
+
+function fileName(request, file, callback){
+    callback(null, file.originalname);
+}
+
+function fileFilter(request, file, callback){
+    if(file.mimeType !== 'image/png'){
+        request.fileValidationError= 'Wrong File Type';
+        callback(null, false, new Error(request.fileValidationError));
+    }else{
+        callback(null, true);
+    }
+}
