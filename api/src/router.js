860cd86278f8f4c8528f2b5521786646c900d921
Mark Buynoski <Mark.Buynoski@transport4.com>
2020-12-23 11:57:05 -0500

    attempt 7

--- api/src/router.js
+++ api/src/router.js
@@ -26,10 +26,7 @@ const upload = multer({
 });
 
 router.post('/upload', upload.single('photo'),(request, response) =>{
-    if(request.fileValidationError){
-        return response.status(400).json({error: request.fileValidationError});
-    }
-    
+    if(request.fileValidationError) return response.status(400).json({error: request.fileValidationError});    
     response.status(201).json({success: true});
 });
 
