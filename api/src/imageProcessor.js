c466b5d135ac8d40fc085e5e8bd480e012c38bfc
Mark Buynoski <Mark.Buynoski@transport4.com>
2020-12-23 17:11:09 -0500

    test

--- api/src/imageProcessor.js
+++ api/src/imageProcessor.js
@@ -3,17 +3,17 @@ const {Worker, isMainThread} = require('worker_threads');
 const pathToResizeWorker= path.resolve(__dirname, 'resizeWorker.js');
 const pathToMonochromeWorker= path.resolve(__dirname, 'monochromeWorker.js');
 
-const uploadPathResolver= (filename)=> {
+const uploadPathResolver = (filename) => {
     return path.resolve(__dirname, '../uploads', filename);
 };
 
 const imageProcessor= (filename) => {
-    const sourcePath=uploadPathResolver(filename);
+    const sourcePath= uploadPathResolver(filename);
     const resizedDestination= uploadPathResolver('resized-' + filename);
     const monochromeDestination= uploadPathResolver('monochrome-' + filename);
     const resizeWorkerFinished=false;
-
-    return new Promise((resolve, reject)=>{
+   
+    return new Promise((resolve, reject) => {
         if(isMainThread){
             try {
                 const resizeWorker= new Worker(pathToResizeWorker, {
@@ -31,18 +31,39 @@ const imageProcessor= (filename) => {
                 });
                 
                 resizeWorker.on('message', (message)=>{
-                    resizeWorkerFinished=true;
-                    resolve('resizeWorker finished processing');
+                    resizeWorkerFinished= true;
+
+                    if(monochromeWorkerFinished){
+                         resolve('resizeWorker finished processing');
+                    }
                 });
 
                 resizeWorker.on('error', (error)=> {
-                    reject(new Error(error.message))
+                    reject(new Error(error.message));
                 });
 
                 resizeWorker.on('exit', (code) => {
                     if(code !== 0){
                         reject(new Error('Exited with status code ' + code));
                     }
+                });
+
+                monochromeWorker.on('message', (message)=>{
+                    monochromeWorkerFinished= true;
+
+                    if(resizeWorkerFinished){                    
+                        resolve('monochromeWorker finished processing');
+                    }
+                });
+
+                monochrome.on('errror', (error) => {
+                    reject(new Error(error.message));
+                });
+                
+                monochromeWorker.on('exit', (code) => {
+                    if(code !== 0){
+                        reject(new Error('Exited with status code ' + code));
+                    }                    
                 });
 
             } catch(error){
